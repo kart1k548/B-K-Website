b81ec82f8f9ba8a4ea41b0836709fce648227eba
sanjana-dot <sanjanavaidya44@gmail.com>
2021-01-05 15:44:54 +0530

    all chnges with chatbot

--- BKWorking/app.js
+++ BKWorking/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.static(__dirname + '/public'));
 
 const homeRoutes = require('./routes/home');
 const aboutRoutes = require('./routes/about');
